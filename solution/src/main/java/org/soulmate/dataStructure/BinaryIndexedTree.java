7e13aba91968910e4198dc2a58efb4a1185de8a8
buzhi-kefou <further181216@gmail.com>
2023-01-08 16:24:36 +0800

    2023.1.8

--- solution/src/main/java/org/soulmate/dataStructure/BinaryIndexedTree.java
+++ solution/src/main/java/org/soulmate/dataStructure/BinaryIndexedTree.java
@@ -4,6 +4,23 @@ public class BinaryIndexedTree {
 
     private int[] bit;
 
+    void init(int[] array) {
+        for (int i = 1; i < bit.length; i++) {
+            update(i, array[i - 1]);
+        }
+    }
+
+    // bit[i] = ∑(array[i-lowBit(i)+1,...,i]
+    void init(int[] array, int[] sum) {
+        for (int i = 1; i < array.length; i++) {
+            sum[i] = (array[i] + sum[i - 1]);
+        }
+
+        for (int i = 1; i < bit.length; i++) {
+            bit[i] = sum[i] - sum[i - lowBit(i)];
+        }
+    }
+
     private int lowBit(int idx) {
         return idx & (-idx);
     }
